16868dfe3a99704317da44aa55809329d0e316ed
Alexis <alexis.fereira@payco.co>
2019-11-25 18:58:29 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const css = require('../scss/main.scss');
+
+
+function whichTransitionEvent(){
+    var t,
+        el = document.createElement("fakeelement");
+
+    var transitions = {
+        "transition"      : "transitionend",
+        "OTransition"     : "oTransitionEnd",
+        "MozTransition"   : "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+    }
+
+    for (t in transitions){
+        if (el.style[t] !== undefined){
+            return transitions[t];
+        }
+    }
+}
+
+var transitionEvent = whichTransitionEvent();
+
+function whichAnimationEvent(){
+    var t,
+        el = document.createElement("fakeelement");
+
+    var animations = {
+        "animation"      : "animationend",
+        "OAnimation"     : "oAnimationEnd",
+        "MozAnimation"   : "animationend",
+        "WebkitAnimation": "webkitAnimationEnd"
+    }
+
+    for (t in animations){
+        if (el.style[t] !== undefined){
+            return animations[t];
+        }
+    }
+}
+
+
+
+var animationEvent = whichAnimationEvent();
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false
+    });
+
+    var alttura = $(".ancla").offset().top
+
+    $(".main-container").scroll(function () {
+
+        if($(".main-container").scrollTop() > alttura){
+            $(".menuFlotante").addClass("baja")
+        }else{
+            $(".menuFlotante").removeClass("baja")
+        }
+    })
+})
